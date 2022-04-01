b5110902080ecbcb5db26e708a62869ff8a7b246
Felipe Alves <profelipealves@outlook.com>
2022-04-01 09:52:28 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './style.css';
+import Routes from './routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+export default function App() {
+  return (
+    <div className="app">
+      <Routes/>
+      <ToastContainer autoClose={3000}/>
+    </div>
+  );
+}
+
